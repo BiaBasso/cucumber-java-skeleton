9cf1f10179b697a90843caaef163705cc67f7ef4
Bianca <bibca17@hotmail.com>
2016-06-24 16:26:48 -0300

    adicionando a step do site contato do video

---
+++ src/test/java/cucumber/StepDefinitionsContact.java
@@ -0,0 +1,28 @@
+package cucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinitionsContact {
+
+	@Given("^I am on my zoo website$")
+	public void i_am_on_my_zoo_website() throws Throwable {
+	    
+	}
+
+	@When("^I click on the contact link$")
+	public void i_click_on_the_contact_link() throws Throwable {
+	    
+	}
+
+	@When("^populate the contact form$")
+	public void populate_the_contact_form() throws Throwable {
+	    
+	}
+
+	@Then("^I should be on the contact confirmation page$")
+	public void i_should_be_on_the_contact_confirmation_page() throws Throwable {
+	    
+	}
+}
