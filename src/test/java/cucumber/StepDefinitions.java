49b2d7049521cdd9b07bf95ef00a8d96285ee87d
Bianca <bibca17@hotmail.com>
2016-06-24 15:32:49 -0300

    tirando a parte comentada e mudando o nome as variaveis

--- src/test/java/cucumber/StepDefinitions.java
+++ src/test/java/cucumber/StepDefinitions.java
@@ -1,6 +1,5 @@
 package cucumber;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -8,20 +7,17 @@ import cucumber.api.java.en.When;
 public class StepDefinitions {
 
 	@Given("^I navigated to the zoo website$")
-	public void i_navigated_to_the_zoo_website() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void shouldNavigateToZoo() throws Throwable {
+
 	}
 
 	@When("^I click on the adoption link$")
-	public void i_click_on_the_adoption_link() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void shouldClickOnAdoption() throws Throwable {
+
 	}
 
 	@Then("^I check to see that no animals are available$")
-	public void i_check_to_see_that_no_animals_are_available() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void checkAnimalStringVisible() throws Throwable {
+
 	}
 }
