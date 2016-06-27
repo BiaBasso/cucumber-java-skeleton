abd9b30a6c42e4c006a531d4111259d7567cf4de
Bianca <bibca17@hotmail.com>
2016-06-27 16:25:55 -0300

    alterando poucas coisas no codigo, para fazer ele rodar com o Outline Data Table do cenario

--- src/test/java/cucumber/StepDefinitionsPgTitle.java
+++ src/test/java/cucumber/StepDefinitionsPgTitle.java
@@ -11,29 +11,30 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class StepDefinitionsPgTitle {
-	
+
 	WebDriver driver = null;
-	
+
 	@Given("^I am on the zoo website$")
 	public void shouldNavigateToZoo() throws Throwable {
 		System.setProperty("webdriver.chrome.driver", "/opt/google/chrome/chromedriver");
 		driver = new ChromeDriver();
-		driver.navigate().to("http://192.168.0.22/Projetos/B/Home.html");
+		driver.navigate().to("http://192.168.0.24/Projetos/B/Home.html");
 	}
-
-	@When("^I navigate to \"([^\"]*)\"$")
-	public void shouldClickOnLink(String link) throws Throwable {
-		driver.findElement(By.id(link)).click();
-	}
-
-	@Then("^I check page title is \"([^\"]*)\"$")
-	public void checkPageTitle(String title) throws Throwable {
-		Assert.assertTrue(driver.getTitle().contains(title));
-	}
-
+	
 	@And("^I close the browser$")
 	public void closeBrowser() throws Throwable {
 		driver.close();
 	}
+
+	@When("^I click on ([^\"]*)$")
+	public void ClickOnLink(String link) throws Throwable {
+		driver.findElement(By.id(link)).click();
+	}
+
+	@Then("^I Check I am on ([^\"]*)$")
+	public void checkPageTitle(String title) throws Throwable {
+		Assert.assertTrue(driver.getTitle().contains(title));
+	}
+
 
 }
