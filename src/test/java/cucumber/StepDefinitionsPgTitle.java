e70bde49e27333542ed4610961fc20dff3e4b53d
Bianca <bibca17@hotmail.com>
2016-06-27 13:18:20 -0300

    tirando aquele código grande e deixando o menor, com os parametros, que faz a mesma funcao do codigo grande

--- src/test/java/cucumber/StepDefinitionsPgTitle.java
+++ src/test/java/cucumber/StepDefinitionsPgTitle.java
@@ -16,70 +16,24 @@ public class StepDefinitionsPgTitle {
 	
 	@Given("^I am on the zoo website$")
 	public void shouldNavigateToZoo() throws Throwable {
+		System.setProperty("webdriver.chrome.driver", "/opt/google/chrome/chromedriver");
+		driver = new ChromeDriver();
+		driver.navigate().to("http://192.168.0.22/Projetos/B/Home.html");
 	}
 
 	@When("^I navigate to \"([^\"]*)\"$")
-	public void shouldClickOn(String arg1) throws Throwable {
+	public void shouldClickOnLink(String link) throws Throwable {
+		driver.findElement(By.id(link)).click();
 	}
 
 	@Then("^I check page title is \"([^\"]*)\"$")
-	public void i_check_page_title_is(String arg1) throws Throwable {
+	public void checkPageTitle(String title) throws Throwable {
+		Assert.assertTrue(driver.getTitle().contains(title));
 	}
 
-	@Then("^I close the browser$")
-	public void i_close_the_browser() throws Throwable {
+	@And("^I close the browser$")
+	public void closeBrowser() throws Throwable {
+		driver.close();
 	}
 
-	
-//	@Given("^I am on the zoo website$")
-//	public void i_am_on_the_zoo_website() throws Throwable {
-//		System.setProperty("webdriver.chrome.driver", "/opt/google/chrome/chromedriver");
-//		driver = new ChromeDriver();
-//		driver.navigate().to("http://192.168.0.22/Projetos/B/Home.html");
-//	}
-//
-//	@When("^I navigate to Home$")
-//	public void i_navigate_to_Home() throws Throwable {
-//	    driver.findElement(By.id("home_link")).click();
-//	}
-//
-//	@Then("^I check page title is Home$")
-//	public void i_check_page_title_is_Home() throws Throwable {
-//	    Assert.assertTrue(driver.getTitle().contains("Home"));
-//	}
-//
-//	@When("^I navigate to Adoption$")
-//	public void i_navigate_to_Adoption() throws Throwable {
-//	    driver.findElement(By.id("adoption_link")).click();
-//	}
-//
-//	@Then("^I check page title is Adoption$")
-//	public void i_check_page_title_is_Adoption() throws Throwable {
-//	    Assert.assertTrue(driver.getTitle().contains("Adoption"));
-//	}
-//
-//	@When("^I navigate to About$")
-//	public void i_navigate_to_About() throws Throwable {
-//	    driver.findElement(By.id("about_link")).click();
-//	}
-//
-//	@Then("^I check page title is About$")
-//	public void i_check_page_title_is_About() throws Throwable {
-//	    Assert.assertTrue(driver.getTitle().contains("About"));
-//	}
-//
-//	@When("^I navigate to Contact$")
-//	public void i_navigate_to_Contact() throws Throwable {
-//	   driver.findElement(By.id("contact_link")).click();
-//	}
-//
-//	@Then("^I check page title is Contact$")
-//	public void i_check_page_title_is_Contact() throws Throwable {
-//	    Assert.assertTrue(driver.getTitle().contains("Contact"));
-//	}
-//	
-//	@And("^I close the browser$")
-//	public void i_close_the_browser() throws Throwable {
-//	    driver.close();
-//	}
 }
