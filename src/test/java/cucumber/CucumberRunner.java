27ae6b1e5df14f2fa7bb71e9b30e61ac2dfb3c46
Bianca <bibca17@hotmail.com>
2016-06-27 18:22:26 -0300

    comentando a linha que tinha erro

--- src/test/java/cucumber/CucumberRunner.java
+++ src/test/java/cucumber/CucumberRunner.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(
-		format = {"pretty", "json:target/"},
-		features = {"src/test/java/cucumber"})
+//@Cucumber.Options(
+//		format = {"pretty", "json:target/"},
+//		features = {"src/test/java/cucumber"})
 public class CucumberRunner {
 
 }
