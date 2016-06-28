b498e2cd41229b39d737d3157302417276675f2d
Bianca <bibca17@hotmail.com>
2016-06-28 15:33:29 -0300

    inserindo o metodo tags, para ele rodar apenas o cenario que coloquei a tag respectiva

--- src/test/java/testes/CucumberRunnerTest.java
+++ src/test/java/testes/CucumberRunnerTest.java
@@ -5,6 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/html/"}, features = "src/test/resources/testes")
+@CucumberOptions(plugin = {"pretty", "html:target/html/"}, 
+	features = "src/test/resources/testes",
+	tags = {"@Application"})
 public class CucumberRunnerTest {
 }
