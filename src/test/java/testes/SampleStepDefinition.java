09a47dd75d7a78174560bd99215c7747a810304c
Bianca <bibca17@hotmail.com>
2016-06-28 17:38:27 -0300

    mudando o codigo para funcionar, usando sleep para esperar o ajax carregar

--- src/test/java/testes/SampleStepDefinition.java
+++ src/test/java/testes/SampleStepDefinition.java
@@ -17,29 +17,32 @@ public class SampleStepDefinition {
 	public void i_am_on_the_zoo_websites() throws Throwable {
 		System.setProperty("webdriver.chrome.driver", "/opt/google/chrome/chromedriver");
 		driver = new ChromeDriver();
-		driver.navigate().to("http://192.168.0.24/Projetos/B/Home.html");
+		driver.navigate().to("http://192.168.0.24/Projetos/B/");
 	}
 
 	@When("^I click onn \"([^\"]*)\"$")
 	public void i_click_onn(String link) throws Throwable {
+		Thread.sleep(5000);
 		driver.findElement(By.id(link.toLowerCase() + "_link")).click();
 	}
 
 	@Then("^I set the start date to \"([^\"]*)\"$")
 	public void i_set_the_start_date_to(String dropItem) throws Throwable {
+		Thread.sleep(5000);
 		Select dropDown = new Select(driver.findElement(By.id("start_select")));
 		dropDown.selectByVisibleText(dropItem);
 	}
 	
-	@Then("^I close the browser$")
-	public void closeBrowser() throws Throwable {
+	@Then("^I quit the browser$")
+	public void quitBrowser() throws Throwable {
 		driver.quit();
 	}
 
 	@Then("^I populate the contact form$")
 	public void i_populate_the_contact_form() throws Throwable {
+		Thread.sleep(5000);
 		driver.findElement(By.name("name_field")).sendKeys("name test");
-		driver.findElement(By.id("rdona")).click();
-		driver.findElement(By.id("cadop")).click();
+		driver.findElement(By.id("checkbox-1")).click();
+		driver.findElement(By.id("option-2")).click();
 	}
 }
