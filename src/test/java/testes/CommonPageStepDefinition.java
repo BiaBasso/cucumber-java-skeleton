d7eabe0d9b378ee01629a298514dae1dfd14db67
Bianca <bibca17@hotmail.com>
2016-06-28 18:58:49 -0300

    fazendo a pagina ter a extensao da AbstractPageDefinition

--- src/test/java/testes/CommonPageStepDefinition.java
+++ src/test/java/testes/CommonPageStepDefinition.java
@@ -2,20 +2,17 @@ package testes;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class CommonPageStepDefinition {
-
-	WebDriver driver;
+public class CommonPageStepDefinition extends AbstractPageStepDefinition {
+	
+	WebDriver driver = getDriver();
 
 	@Given("^I am on the zoo websites$")
 	public void i_am_on_the_zoo_websites() throws Throwable {
-		System.setProperty("webdriver.chrome.driver", "/opt/google/chrome/chromedriver");
-		driver = new ChromeDriver();
 		driver.navigate().to("http://192.168.0.24/Projetos/B/");
 	}
 
