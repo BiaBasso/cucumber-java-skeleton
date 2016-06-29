ef1d3eace21007e284194127ca5d5f138e626f07
Bianca <bibca17@hotmail.com>
2016-06-29 15:08:39 -0300

    mudando o codigo para mais simples, linkando ele com outras paginas que fazem a mesma funcao do que o codigo inteiro antes, deixando mais simples a manutencao do codigo

--- src/test/java/testes/StepDefinitionZoo.java
+++ src/test/java/testes/StepDefinitionZoo.java
@@ -1,6 +1,5 @@
 package testes;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import cucumber.api.java.en.And;
@@ -8,59 +7,66 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import junit.framework.Assert;
+import testes.pageObject.ContactConfirmPage;
+import testes.pageObject.ContactPage;
+import testes.pageObject.LandingPage;
 
-public class StepDefinitionZoo extends AbstractPageStepDefinition{
-	
-	
+public class StepDefinitionZoo extends AbstractPageStepDefinition {
+
 	WebDriver driver = getDriver();
+	LandingPage landingPage;
+	ContactPage contactPage;
+	ContactConfirmPage contactConfirmPage;
+	
 
 	@Given("^I stay on the zoo websites$")
 	public void i_stay_on_the_zoo_websites() throws Throwable {
-	    driver.navigate().to("http://192.168.0.24/Projetos/B/");
+		landingPage = new LandingPage(driver);
+		landingPage.navigateToAdocaoZoo();
 	}
 
 	@When("^I click the \"([^\"]*)\"$")
 	public void i_click_the(String link) throws Throwable {
-		Thread.sleep(5000);
-	    driver.findElement(By.id(link.toLowerCase() + "_link")).click();
+		 Thread.sleep(5000);
+		 contactPage = landingPage.navigateToContactPage(link);
 	}
 
 	@And("^I enter \"([^\"]*)\" into the name field$")
 	public void i_enter_into_the_name_field(String value) throws Throwable {
-		Thread.sleep(1000);
-	    driver.findElement(By.name("name_field")).sendKeys(value);
+		 Thread.sleep(1000);
+		 contactPage.setNameField(value);
 	}
 
 	@And("^I enter \"([^\"]*)\" into the address field$")
 	public void i_enter_into_the_address_field(String value) throws Throwable {
-		Thread.sleep(1000);
-		driver.findElement(By.name("address_field")).sendKeys(value);
+		 Thread.sleep(1000);
+		 contactPage.setAddressField(value);
 	}
 
 	@And("^I enter \"([^\"]*)\" into the postcode field$")
 	public void i_enter_into_the_postcode_field(String value) throws Throwable {
-		Thread.sleep(1000);
-		driver.findElement(By.name("postcode_field")).sendKeys(value);
+		 Thread.sleep(1000);
+		contactPage.setPostcodeField(value);
 	}
 
 	@And("^I enter \"([^\"]*)\" into the email field$")
 	public void i_enter_into_the_email_field(String value) throws Throwable {
-		Thread.sleep(1000);
-		driver.findElement(By.name("email_field")).sendKeys(value);
+		 Thread.sleep(1000);
+		 contactPage.setEmailField(value);
 	}
 
 	@And("^I submit the contact form$")
 	public void i_submit_the_contact_form() throws Throwable {
-	    driver.findElement(By.id("submit_message")).click();
+		contactConfirmPage = contactPage.submitForm();
 	}
 
 	@Then("^I checked I am on the confirmation page$")
 	public void i_checked_I_am_on_the_confirmation_page() throws Throwable {
-	    Assert.assertTrue(driver.findElement(By.cssSelector(".content h1")).getText().contains("We have your message"));
+		Assert.assertTrue(contactConfirmPage.getPageTitle().contains("We have your message"));
 	}
 
 	@And("^I closed the browser$")
 	public void i_closed_the_browser() throws Throwable {
-	    driver.quit();
+		contactConfirmPage.closeDriver();
 	}
 }
