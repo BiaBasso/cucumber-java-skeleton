25e608015e36172860c595a006bc5979f435e3d6
Bianca <bibca17@hotmail.com>
2016-06-29 15:10:00 -0300

    inserindo a classe contendo o inicio e o final do codigo, ou seja, ele abre a pagina e fecha a pagina do navegador

---
+++ src/test/java/testes/pageObject/AbstractPage.java
@@ -0,0 +1,21 @@
+package testes.pageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class AbstractPage {
+
+protected WebDriver driver;
+	
+	public AbstractPage (WebDriver driver){
+		this.driver = driver;
+	}
+	
+	public LandingPage navigateToAdocaoZoo(){
+		driver.navigate().to("http://192.168.0.24/Projetos/adocaozoo/");
+		return new LandingPage(driver);
+	}
+	
+	public void closeDriver() {
+		driver.quit();
+	}
+}
