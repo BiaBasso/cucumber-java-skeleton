8d0ad031f73fa34df75190a10b23a39b2943db4b
Bianca <bibca17@hotmail.com>
2016-06-29 15:13:06 -0300

    classe que vai para a pagina de confirmacao, depois de ser clicado o botao submit

---
+++ src/test/java/testes/pageObject/ContactConfirmPage.java
@@ -0,0 +1,15 @@
+package testes.pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ContactConfirmPage extends ContactPage{
+
+	public ContactConfirmPage(WebDriver driver){
+		super(driver);
+	}
+	
+	public String getPageTitle() {
+		return driver.findElement(By.cssSelector(".content h1")).getText();
+	}
+}
