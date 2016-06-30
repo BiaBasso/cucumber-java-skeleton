52a31c781fee14df31694955dd841540ee02dc33
Bianca <bibca17@hotmail.com>
2016-06-30 17:43:07 -0300

    mudando o runner de junit para testng

--- src/test/java/testes/CucumberRunnerAllFeaturesTest.java
+++ src/test/java/testes/CucumberRunnerAllFeaturesTest.java
@@ -1,12 +1,10 @@
 package testes;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/html/"}, 
 	features = "src/test/resources/testes/features")
-public class CucumberRunnerAllFeaturesTest {
+public class CucumberRunnerAllFeaturesTest extends AbstractTestNGCucumberTests {
 
 }
